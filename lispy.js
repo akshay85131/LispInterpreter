278555c0cda63e0635817b442e77464cff33b2ff
Akshay Tulajannavar <akshay.tulajannavar@mountblue.io>
2019-03-07 10:21:56 +0530

    ifParser  implemented

--- lispy.js
+++ lispy.js
@@ -15,6 +15,13 @@ function numberParser (value) {
   else return null
 }
 
+function stringParser (str) {
+  let regEx = /^[A-Za-z]+/
+  let value = str.match(regEx)
+  if (value !== null) return [value[0], str.slice(value[0].length, str.length)]
+  else return null
+}
+
 let Env = {
   '+': function add (input) {
     return input.reduce((acc, cur) => {
@@ -41,20 +48,47 @@ let Env = {
     return input.reduce((acc, cur) => {
       return parseInt(acc % cur)
     })
-  }
+  },
+  '<': function lessThan (input) {
+    return input.reduce((acc, cur) => {
+      return parseInt(acc < cur)
+    })
+  },
+  '>': function greaterThan (input) {
+    return input.reduce((acc, cur) => {
+      return (acc > cur)
+    })
+  },
+  '=': function equal (input) {
+    return input.reduce((acc, cur) => {
+      return (acc = cur)
+    })
+  },
+  '>=': function greatAndEqual (input) {
+    return input.reduce((acc, cur) => {
+      return (acc >= cur)
+    })
+  },
+  '<=': function lessAndEEquak (input) {
+    return input.reduce((acc, cur) => {
+      return (acc <= cur)
+    })
+  },
+  'pi': Math.PI
 }
 
 function evalExpressions (input) {
+  input = removeSpace(input)
   if (!input.startsWith('(')) return null
   input = input.slice(1)
   input = removeSpace(input)
   let result = identifier(input)
-  if (Env.hasOwnProperty(result)) { return operator(input) }
+  if (Env.hasOwnProperty(result)) { return operator(input) } else { return specialFunction(input) }
 }
-console.log(evalExpressions('( + 1 (+ 1 2) ( + 1 1))'))
+console.log(evalExpressions('(if (> 2 1 ) (+ 1 (+ 1 1 ) ) (+ 1 3 ) )'))
 
 function identifier (input) {
-  let result = input.slice(0, 1)
+  let result = input.slice(0, input.indexOf(' '))
   return result
 }
 
@@ -78,4 +112,28 @@ function operator (input) {
     }
   }
   return Env[result](array)
+}
+
+/// ///////// Special Functions ////////////
+
+function specialFunction (input) {
+  if (input.startsWith('if')) return ifParser(input)
+  if (input.startsWith('define')) return parserDef(input)
+}
+
+/// /// if //////////////
+
+function ifParser (input) {
+  input = input.slice(2)
+  input = removeSpace(input)
+
+  let condiCheck = evalExpressions(input)
+  input = input.slice((input.indexOf(')') + 1), input.length)
+  input = removeSpace(input)
+  let value1 = evalExpressions(input)
+  input = input.slice((input.indexOf(')') + 1), input.length)
+  input = removeSpace(input)
+  let value2 = evalExpressions(input)
+
+  if (condiCheck === true) { return value1 } else { return value2 }
 }
