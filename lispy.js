4f1deeb6536c1c93ba3b424f84ba77d6b691b49e
Akshay Tulajannavar <akshay.tulajannavar@mountblue.io>
2019-03-19 19:54:53 +0530

    lambda working for twice and circle area

--- lispy.js
+++ lispy.js
@@ -1,4 +1,5 @@
 // let readline = require('readline')
+let local
 function removeSpace (input) {
   var first = input.search(/\S/)
   if (first === -1) {
@@ -8,9 +9,7 @@ function removeSpace (input) {
 }
 function numberParser (value) {
   let expression = /^[-]?[0-9]+(\.[0-9]+(?:[Ee][+-]?[0-9]+)?)?/
-  //  /^[+-]?([0-9]*\.?[0-9]+|[0-9]+\.?[0-9]*)([eE][+-]?[0-9]+)?/
   let result = value.match(expression)
-  // console.log(result)
   if (result !== null) return [parseInt(result[0]), value.slice(result[0].length, value.length)]
   else return null
 }
@@ -19,15 +18,11 @@ function stringParser (input) {
   let regEx = /^[A-Za-z]+/.exec(input)
   let value = input.match(regEx)
   if (value === null) { return null }
-  // if (env.hasOwnProperty(value[0])) {
-  //   return [env[value[0]], input.slice(value[0].length, input.length)]
-  // }
   return [value[0], input.slice(value[0].length, input.length)]
 }
 let env = {
   '+': function add (input) {
     return input.reduce((acc, cur) => {
-      // console.log(cur)
       return parseFloat(acc + cur)
     })
   },
@@ -81,6 +76,7 @@ let env = {
   },
   'pi': Math.PI
 }
+// let current = env
 
 function ifParser (input) {
   input = input.slice(1)
@@ -94,33 +90,29 @@ function ifParser (input) {
   input = input.slice((input.indexOf(')') + 1), input.length)
   input = removeSpace(input)
   let conseq = mainFunc(input)
-  input = input.slice((input.indexOf(')') + 1), input.length)
-  input = removeSpace(input)
-  let alt = mainFunc(input)
   if (condiCheck[0] === true) {
     return conseq[0]
   } else {
+    input = input.slice(conseq.length)
+    // input = input.slice((input.indexOf(')') + 1), input.length)
+    input = removeSpace(input)
+    let alt = mainFunc(input)
     return alt[0]
   }
 }
 function defineParser (input) {
   input = input.slice(1)
-  // input = removeSpace(input)
   if (!input.startsWith('define')) {
     return null
   }
   input = input.slice(6)
   input = removeSpace(input)
   let key = stringParser(input)
-  // input = inpu(1)
   input = removeSpace(key[1])
-  // if (input.startsWith('(')) input = mainFunc(input)
   let defvalue = mainFunc(input)
   env[key[0]] = defvalue[0]
-  //   console.log(env)
   return 'property added in env'
 }
-
 function quoteParser (input) {
   input = input.slice(1)
   input = removeSpace(input)
@@ -151,23 +143,23 @@ function lambdaParser (input) {
     return null
   }
   let localEnv = {}
-  input = input.slice('6')
+  input = input.slice(6)
   input = removeSpace(input)
-  localEnv['env'] = {}
-  localEnv['env']['parent'] = env
-  localEnv['env']['args'] = {}
+  localEnv['envs'] = {}
+  localEnv['envs']['parent'] = env
+  localEnv['envs']['args'] = {}
   if (input.startsWith('(')) {
     input = input.slice(1)
     while (!input.startsWith(')')) {
       let argu = stringParser(input)
-      localEnv['env']['args'][argu[0]] = null
+      localEnv['envs']['args'][argu[0]] = null
       input = input.slice(argu[0].length)
     }
     input = input.slice(1)
     input = removeSpace(input)
-    localEnv['env']['expression'] = input
+    localEnv['envs']['expression'] = input
   }
-  return [localEnv, input.slice(localEnv['env']['expression'])]
+  return [localEnv, input.slice(localEnv['envs']['expression'])]
 }
 
 function symbolParser (input) {
@@ -176,14 +168,40 @@ function symbolParser (input) {
   let result = stringParser(input)
   if (result === null) return null
   if (env.hasOwnProperty(result[0])) {
-    if (typeof (result[0]) === 'object') {
-      return evalLambda(input)
+    if (typeof env[result[0]] === 'object') {
+      return updateAndEvalLambda(input)
     }
     return [env[result[0]], result[1]]
-  } else return null
+  } else {
+    if (local['args'].hasOwnProperty(result[0])) {
+      return [local['args'][result[0]], result[1]]
+    }
+  }
+  return null
+}
+function updateAndEvalLambda (input) {
+  input = removeSpace(input)
+  let keyElm = stringParser(input)
+  let current = env
+  current = current[keyElm[0]]['envs']
+  local = current
+  input = input.slice(keyElm[0].length)
+  input = removeSpace(input)
+  while (!input.startsWith(')')) {
+    for (let key in current['args']) {
+      let result = mainFunc(input)
+      current['args'][key] = result[0]
+      // result = result[0].toString()
+      input = removeSpace(result[1])
+      // input = removeSpace(input)
+    }
+  }
+  return evalLambda(current)
 }
 
-function evalLambda (input) {
+function evalLambda (obj) {
+  let result = mainFunc(obj['expression'])
+  return result
 }
 
 function mainFunc (input) {
@@ -204,22 +222,22 @@ function evalExpressions (input) {
   input = input.slice(1)
   input = removeSpace(input)
   let procedure = input.slice(0, input.indexOf(' '))
-  input = input.slice(input.indexOf(' '))
-  input = removeSpace(input)
   if (!env.hasOwnProperty(procedure)) {
     return null
   }
+  if (typeof env[procedure] === 'object') {
+    return symbolParser(input)
+  }
+  input = input.slice(input.indexOf(' '))
+  input = removeSpace(input)
   while (!input.startsWith(')')) {
     let values = mainFunc(input)
     args.push(values[0])
     input = removeSpace(values[1])
-    // input = input.slice(String(values[0]).length, input.length)
     input = removeSpace(input)
   }
   input = removeSpace(input.slice(1))
-
   return [env[procedure](args), input]
-  // return env[procedure](args)
 }
 
 // var rl = readline.createInterface(process.stdin, process.stdout)
@@ -235,9 +253,14 @@ function evalExpressions (input) {
 // // console.log(rl)
 
 // console.log(mainFunc('(+ 2 (+ 6 (+ 4 8)))'))
-console.log(mainFunc('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))'))
-console.log(env)
-console.log(mainFunc('(fact 3)'))
-// console.log(mainFunc('(define define 10'))
+// console.log(mainFunc('(define twice (lambda (n) (* 2 n)))'))
 // console.log(env)
-// console.log(mainFunc('(+ define 1)'))
+// console.log(mainFunc('(twice 3)'))
+// console.log(localEnv)
+// console.log(mainFunc('(if (< 10 20) (+ 1  (+ 3 3))'))
+// console.log(env)
+// console.log(mainFunc('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))'))
+// console.log(mainFunc('(fact 2)'))
+// console.log(mainFunc('(define circlearea (lambda (r) (* pi (* r r))))'))
+// console.log(mainFunc('(circlearea 3)'))
+// console.log(mainFunc('(if (> 10 20) 1 (+ 3 3))'))
